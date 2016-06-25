3a2a4b8b9d58e84199049495dde7dd90b7bf662d
meriteshkumar <me.ritesh.kumar@gmail.com>
2016-06-25 13:15:58 +0530

    Offer/Coupon page

---
+++ DealDunia.Domain/Entities/VCommissionResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealDunia.Domain.Entities
+{
+    public class VCommissionResponse
+    {
+        public string featured { get; set; }
+        public object exclusive { get; set; }
+        public string promo_id { get; set; }
+        public int offer_id { get; set; }
+        public string offer_name { get; set; }
+        public string coupon_title { get; set; }
+        public string category { get; set; }
+        public string coupon_description { get; set; }
+        public string coupon_type { get; set; }
+        public string coupon_code { get; set; }
+        public string ref_id { get; set; }
+        public string link { get; set; }
+        public string coupon_expiry { get; set; }
+        public string added { get; set; }
+        public string preview_url { get; set; }
+        public bool status { get; set; }
+        public string store_link { get; set; }
+    }
+}
--- DealDunia.Web/Controllers/OfferController.cs
+++ DealDunia.Web/Controllers/OfferController.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using DealDunia.Domain.Entities;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace DealDunia.Web.Controllers
 {
@@ -10,7 +15,30 @@ namespace DealDunia.Web.Controllers
     {
         public ActionResult Coupon()
         {
-            return View();
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://tools.vcommission.com/api/coupons.php?apikey=4cd38b37164ca7837819c4196b1ff81fae72073a15054c60ba6a50653d97ac6d");
+
+            List<VCommissionResponse> deserializedResult = null;
+
+            try
+            {
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                    string json = reader.ReadToEnd();
+
+                    deserializedResult = serializer.Deserialize<List<VCommissionResponse>>(json);
+                }
+            }
+            catch (Exception ex)
+            {
+                string errorText = ex.Message;
+            }
+
+            return View(deserializedResult);
         }
 
     }
