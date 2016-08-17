b1b766d95260a356fa556e3415e62441d55567cf
meriteshkumar <me.ritesh.kumar@gmail.com>
2016-08-17 14:24:11 +0530

    Store filter updated

--- DealDunia.Web/Areas/Admin/Controllers/CouponsController.cs
+++ DealDunia.Web/Areas/Admin/Controllers/CouponsController.cs
@@ -20,11 +20,16 @@ namespace DealDunia.Web.Areas.Admin.Controllers
             return View(coupons);
         }
 
-        public PartialViewResult _CouponGrid(int storeSourceId = 0, int storeCategoryId = 0, bool? featured = null, bool? status = null)
+        public PartialViewResult _CouponGrid(int storeSourceId = 0, int storeCategoryId = 0, bool? featured = null, bool? status = null, string offerType = null, string store = null)
         {
             EComEntities context = new EComEntities();
 
-            var coupons = context.Coupons.Where(e => (storeSourceId == 0 || e.StoreSourceId == storeSourceId) && (storeCategoryId == 0 || e.StoreCategoryId == storeCategoryId) && (bool)(e.Featured == (featured ?? e.Featured)) && (bool)(e.Status == (status ?? e.Status))).ToList();
+            var coupons = context.Coupons.Where(e => (storeSourceId == 0 || e.StoreSourceId == storeSourceId)
+                                                    && (storeCategoryId == 0 || e.StoreCategoryId == storeCategoryId)
+                                                    && (bool)(e.Featured == (featured ?? e.Featured))
+                                                    && (bool)(e.Status == (status ?? e.Status))
+                                                    && (e.OfferType == (string.IsNullOrEmpty(offerType) ? e.OfferType : offerType))
+                                                    && (e.OfferName.Contains((string.IsNullOrEmpty(store) ? e.OfferName : store)))).ToList();
 
             return PartialView(coupons);
         }
@@ -38,12 +43,24 @@ namespace DealDunia.Web.Areas.Admin.Controllers
             return PartialView(storeSource);
         }
 
+        public PartialViewResult _StoreDropDown(string controlName)
+        {
+            EComEntities context = new EComEntities();
+
+            var store = context.Stores.Join(context.Coupons,
+                s => new { StoreSourceId = (int?)s.StoreSourceId, SourceStoreId = (int?)s.SourceStoreId },
+                c => new { StoreSourceId = (int?)c.StoreSourceId, SourceStoreId = (int?)c.OfferId },
+                (s, c) => new StoreDropDown { StoreName = s.StoreName }).Distinct().ToList();
+
+            return PartialView(store);
+        }
+
         public ActionResult EditCoupon(int couponId, short storeCateogoryId)
         {
             EComEntities context = new EComEntities();
 
             var coupon = context.Coupons.Single(s => s.CouponId == couponId);
-            coupon.StoreCategoryId =  storeCateogoryId;
+            coupon.StoreCategoryId = storeCateogoryId;
             context.SaveChanges();
 
             TempData["Message"] = "Record Updated";
@@ -51,5 +68,24 @@ namespace DealDunia.Web.Areas.Admin.Controllers
             return RedirectToAction("Index");
         }
 
+        public static string GetStoreName(short StoreSourceId, int OfferId)
+        {
+            EComEntities context = new EComEntities();
+            string storeName;
+
+            var st = context.Stores.FirstOrDefault(s => s.StoreSourceId == StoreSourceId && s.SourceStoreId == OfferId);
+
+            if (st != null)
+                storeName = st.StoreName;
+            else
+                storeName = "N/A";
+
+            return storeName;
+        }
+    }
+
+    public class StoreDropDown
+    {
+        public string StoreName { get; set; }
     }
 }
