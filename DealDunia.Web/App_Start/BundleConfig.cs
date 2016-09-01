7b4269117679e3af9f639557a61f508864755afe
sandeepmittal11 <sandeep.mittal11@gmail.com>
2016-09-01 11:25:23 +0530

    cdn and css sprite

--- DealDunia.Web/App_Start/BundleConfig.cs
+++ DealDunia.Web/App_Start/BundleConfig.cs
@@ -7,17 +7,24 @@ namespace DealDunia.Web
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/*.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/*.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/js")
-                .Include(
-                //"~/Scripts/jquery-{version}.js",
-                //"~/Scripts/jquery-ui.min.js",
-                //"~/Scripts/jquery.validate.min.js",
-                //"~/Scripts/jquery.validate.unobtrusive.min.js",
-                //"~/Scripts/jquery.unobtrusive-ajax.min.js",
-                "~/Scripts/jquery-2.2.3.min.js",
-                "~/Scripts/bootstrap.min.js"));   
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/category.css",
+                "~/Content/menu.css",
+                "~/Content/sidebar-menu.css",
+                "~/Content/style.css"
+                ));
+
+            //bundles.Add(new ScriptBundle("~/bundles/js")
+            //    .Include(
+            //    //"~/Scripts/jquery-{version}.js",
+            //    //"~/Scripts/jquery-ui.min.js",
+            //    //"~/Scripts/jquery.validate.min.js",
+            //    //"~/Scripts/jquery.validate.unobtrusive.min.js",
+            //    //"~/Scripts/jquery.unobtrusive-ajax.min.js",
+            //    "~/Scripts/jquery-2.2.3.min.js",
+            //    "~/Scripts/bootstrap.min.js"));   
         }
     }
 }
