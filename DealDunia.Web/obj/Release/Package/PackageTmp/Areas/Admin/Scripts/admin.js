00c8ed19438a8f10380d61eac9d929a8a8e957fe
sandeepmittal11 <sandeep.mittal11@gmail.com>
2016-10-17 15:19:44 +0530

    design issue resolved

---
+++ DealDunia.Web/obj/Release/Package/PackageTmp/Areas/Admin/Scripts/admin.js
@@ -0,0 +1,24 @@
+﻿function callAjaxURLWithSingleParam(url, param) {
+    return $.ajax({
+        url: url,
+        data: param
+    });
+}
+
+function callAjaxPostURLWithSingleParam(url, param) {
+    return $.ajax({
+        url: url,
+        data: param,
+        type: 'POST'
+    });
+}
+
+function callAjaxPostURLWithSingleParamJSON(url, param) {
+    return $.ajax({
+        url: url,
+        data: param,
+        datatype:'Json',
+        type: 'POST'
+    });
+}
+
